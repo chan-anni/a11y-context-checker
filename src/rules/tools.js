3d38a78ad96fa48f8bd020ad81f1d2a59cccb3e6
Annika Chan <annikac2006@gmail.com>
2026-06-15 23:54:37 -0700

    Fix: updating test case and import call

--- src/rules/tools.js
+++ src/rules/tools.js
@@ -1,7 +1,7 @@
 // Rules tool kit, allows for plugging in and removing any tools/tool scripts
 
 import wcag244 from './wcag_244.js';
-import wcag314 from './wcag314.js';
+import wcag314 from './wcag_314.js';
 import wcag131 from './wcag131.js';
 
 export const rules = [
